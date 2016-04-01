b398cc10c476644f8ea4c4c058896418f056d071
Pierre GILLON <gillon.pierre69@gmail.com>
2016-04-01 17:37:00 +0200

    Iteration 1 : Pommes, Banane, Cerise.

--- src/main/java/fr/valtech/FruitShop.java
+++ src/main/java/fr/valtech/FruitShop.java
@@ -1,19 +1,33 @@
 package fr.valtech;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FruitShop {
+    private Map<String, Integer> catalog = new HashMap<String, Integer>();
+    private int total;
+
+    public FruitShop(){
+        this.catalog.put("Pommes", 100);
+        this.catalog.put("Cerises", 75);
+        this.catalog.put("Bananes", 150);
+    }
 
     /**
      * Add a fruit to the cart
      */
-    public void addFruit(String fruit){
-        //TODO add some code here
+    public void addFruit(String fruit) {
+        if(catalog.containsKey(fruit)){
+            this.total += this.catalog.get(fruit);
+        }
     }
 
     /**
      * The total fruit prices
+     *
      * @return the total cart price
      */
-    public int getTotal(){
-        return 0;
+    public int getTotal() {
+        return this.total;
     }
 }
--- src/test/java/fr/valtech/FruitShopTest.java
+++ src/test/java/fr/valtech/FruitShopTest.java
@@ -1,21 +1,52 @@
 package fr.valtech;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FruitShopTest {
+    private FruitShop _fruitShop;
+
+    @Before
+    public void Initialize() {
+        _fruitShop = new FruitShop();
+    }
 
     @Test
-    public void should_return_one_apple_price(){
-        //Given
-        FruitShop fruitShop = new FruitShop();
+    public void should_have_total_to_100_when_adding_Pommes_once() {
+        // When
+        _fruitShop.addFruit("Pommes");
 
-        //When
-        fruitShop.addFruit("pommes");
+        // Then
+        assertThat(_fruitShop.getTotal()).isEqualTo(100);
+    }
 
-        //Then
-        assertThat(fruitShop.getTotal()).isEqualTo(100);
+    @Test
+    public void should_have_total_to_175_when_adding_Cerises_once() {
+        // When
+        _fruitShop.addFruit("Cerises");
+
+        // Then
+        assertThat(_fruitShop.getTotal()).isEqualTo(75);
+    }
+
+    @Test
+    public void should_have_total_to_175_when_adding_Bananes_once() {
+        // When
+        _fruitShop.addFruit("Bananes");
+
+        // Then
+        assertThat(_fruitShop.getTotal()).isEqualTo(150);
+    }
+
+    @Test
+    public void should_have_total_of_150_when_adding_Cerises_twice() {
+        // When
+        _fruitShop.addFruit("Cerises");
+        _fruitShop.addFruit("Cerises");
+
+        // Then
+        assertThat(_fruitShop.getTotal()).isEqualTo(150);
     }
 }
